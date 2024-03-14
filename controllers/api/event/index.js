ec44df350c42cf1a7116e0cc6954214f6b090a96
00016470 <wiut16470@gmail.com>
2024-03-14 08:59:05 +0500

    CRUD, create is done

--- controllers/api/event/index.js
+++ controllers/api/event/index.js
@@ -5,7 +5,11 @@ const event_service = require('../../../services/event/')
 const event_controller = {
     getAll(req, res) {
         res.json(event_service.getAll())
-    }
+    },
+    create(req, res) {
+        res.status(201).json(
+        event_service.create(req, res))
+        }
 }
 
 module.exports = event_controller
