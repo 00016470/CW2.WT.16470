6bf05bd569a0026f8c51a3069234ddea202bfca4
00016470 <wiut16470@gmail.com>
2024-03-14 07:54:47 +0500

    Add and update pages created, without functionality

--- routes/web/home/index.js
+++ routes/web/home/index.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const home_controller = require('../../../controllers/web/home');
 //redirect to localhost:3000/
 router.get('/', home_controller.index);
+//redirect to localhost:3000/add
+router.get('/add', home_controller.add);
+//redirect to localhost:3000/update
+router.get('/update', home_controller.update);
 
 module.exports = router;
